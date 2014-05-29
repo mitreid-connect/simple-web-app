d0ad98d8a652e0441948f09a1e0b417336d1face
Justin Richer <jricher@mit.edu>
2014-05-29 10:00:27 -0400

    updated to 1.1.6, ran cleanup/copyright on codebase

--- src/main/java/org/mitre/web/HomeController.java
+++ src/main/java/org/mitre/web/HomeController.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2014 The MITRE Corporation
+ *   and the MIT Kerberos and Internet Trust Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package org.mitre.web;
 
 import java.security.Principal;
@@ -18,46 +34,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model, Principal p) {
 		logger.info("Welcome home! the client locale is "+ locale.toString());
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
 		model.addAttribute("serverTime", formattedDate );
-		
+
 		return "home";
 	}
-	
+
 	@RequestMapping("/user")
 	@PreAuthorize("hasRole('ROLE_USER')")
 	public String user(Principal p) {
 		return "user";
 	}
-	
+
 	@RequestMapping("/open")
 	public String open(Principal p) {
 		return "open";
 	}
-	
+
 	@RequestMapping("/admin")
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public String admin(Principal p) {
 		return "admin";
 	}
-	
+
 	@RequestMapping("/login")
 	public String login(Principal p) {
-		return "login";	
+		return "login";
 	}
-	
+
 }
